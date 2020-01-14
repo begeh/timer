d91f0476b5a8f556e8541ab0c03f70d5fb2ba3af
Bashir Egeh <b_egeh@hotmail.com>
2020-01-14 18:19:30 +0000

    first commit timer1.js

---
+++ timer1.js
@@ -0,0 +1,9 @@
+let args = process.argv.slice(2);
+for (let i = 0; i < args.length; i++) {
+  if (args[i] >= 0 && !(Number.isNaN(args[i]))) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      console.log(`beep at ${args[i]} seconds`);
+    }, 1000 * args[i]);
+  }
+}

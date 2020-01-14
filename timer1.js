840404f3fdecfa88e0aae3137dede5dcbfad9fcb
Bashir Egeh <b_egeh@hotmail.com>
2020-01-14 18:40:18 +0000

    edits

--- timer1.js
+++ timer1.js
@@ -1,9 +1,11 @@
 let args = process.argv.slice(2);
 for (let i = 0; i < args.length; i++) {
-  if (args[i] >= 0 && typeof Number(args[i]) == 'number') {
+  if (args[i] >= 0 && !(Number.isNaN(args[i]))) {
     setTimeout(() => {
       process.stdout.write('\x07');
       console.log(`beep at ${args[i]} seconds`);
     }, 1000 * args[i]);
   }
 }
+
+typeof Number(args[i]) == 'number'
